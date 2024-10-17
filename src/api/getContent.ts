4ba26d8890dc73e5326ea5c5e18dcafeb149d011
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-10-17 17:20:39 -0400

    CORS issue fix

--- src/api/getContent.ts
+++ src/api/getContent.ts
@@ -7,7 +7,28 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
 
+// List of allowed origins
+const allowedOrigins = ["https://full-r3f.webflow.io"];
+
 export default async (req: VercelRequest, res: VercelResponse) => {
+  // Get the origin of the request
+  const origin = req.headers.origin;
+
+  const safeOrigin = origin || "null";
+  if (allowedOrigins.includes(safeOrigin)) {
+    res.setHeader("Access-Control-Allow-Origin", safeOrigin);
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "null");
+  }
+
+  if (req.method === "OPTIONS") {
+    // Handle preflight OPTIONS request
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.status(204).end();
+    return;
+  }
+
   if (req.method === "GET") {
     try {
       // Retrieve the data from Redis
@@ -16,7 +37,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
       if (cachedData) {
         res.setHeader("Content-Type", "application/json");
         res.setHeader("Cache-Control", "public, max-age=0, must-revalidate");
-        res.setHeader("Access-Control-Allow-Origin", "*"); // Replace '*' with your domain for security
         res.status(200).send(cachedData);
       } else {
         res.status(503).json({ error: "Content not available" });
@@ -26,6 +46,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
       res.status(500).send("Internal Server Error");
     }
   } else {
+    res.setHeader("Allow", "GET, OPTIONS");
     res.status(405).send("Method Not Allowed");
   }
 };
